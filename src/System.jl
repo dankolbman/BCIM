0fc990c8a7052ab966d7c16e752537aa51b8c0b3
Dan Kolbman <dankolbman@gmail.com>
2015-01-30 14:14:07 -0500

    Add CellGrid type for cellgrid abstraction

--- src/System.jl
+++ src/System.jl
@@ -1,13 +1,19 @@
 type System
   parts::Array{Part,1}
-  cellGrid::Array{Cell}
+  cellGrid::CellGrid
   dimConst::DimensionlessConst
 end
 
 # Construct a system given particle counts and dimensionless parameters
 function System(dc::DimensionlessConst)
+  # The minimum size of the cells
+  cellSize = dc.dia*(2*dc.contact+1)
+  # The minimum number of cells of the given size
+  cellNum = int(2*dc.size/cellSize)
+  # The actual size of the cells
+  cellSize = 2*dc.size/cellNum
   p = constructParts(dc)
-  c = constructCells(dc)
+  c = constructCells(cellNum, cellSize)
 
   return System(p, c, dc)
 end
@@ -20,7 +26,7 @@ end
 function uniformSphere(dc::DimensionlessConst)
   parts = Array(Part,sum(dc.npart))
   pl = 1
-  for sp in 1:length(sum(dc.npart))
+  for sp in 1:length(dc.npart)
     for p in 1:dc.npart[sp]
       # This creates a uniform distribution in the sphere
       lam = (dc.size-dc.dia/2.0)*cbrt(rand())
@@ -34,17 +40,76 @@ function uniformSphere(dc::DimensionlessConst)
   return parts
 end
 
-# Make cells
-function constructCells(dc::DimensionlessConst)
-  return Array(Cell,1)
+
+# Initialize neighbor cells
+function constructCells(D::Int, cellSize::Float64)
+  tCell = D*D*D
+  cells = Array(Cell, D, D, D)
+  # Create cells
+  for i in 1:tCell
+    cell = Cell(i, Array(Cell,0), Array(Part,0))
+    cells[i] = cell
+  end
+  # Link cells
+  for dep in 1:D
+    for col in 1:D
+      for row in 1:D
+        n = [cells[row,col,dep]]         # Add self reference
+        for d in -1:1
+          for c in -1:1
+            for r in -1:1
+              if( d != 0 && c != 0 && r != 0)
+                if(getNeighbor(cells, r, c,d, D) != None)
+                  push!(n, getNeighbor(cells, r, c, d, D))   # Beautiful
+                end
+              end
+            end
+          end
+        end
+        cells[row,col,dep].neighbors = n # Neighbor list update
+      end
+    end
+  end
+  return CellGrid(cells, D, cellSize)
 end
 
-################################################################################
+# Gets a cell at the given location after checking that it is in bounds
+# Parameters:
+#   cellsGrid - The 3D grid of cells to get a cell from
+#   row - The row coordinate
+#   col - The col coordinate
+#   dep - The depth cordinate
+#   D - The number of cells per dimension
+# Returns:
+#   A cell or None if request coord is out of bounds
+function getNeighbor( cells::Array{Cell}, row, col, dep, D )
+  if( row > 0 && col > 0 && dep > 0 && row <= D && col <= D && dep <= D)
+    return cells[row, col, dep]
+  else
+    return None
+  end
+end
+
+# Assign particles to cells
+# Params
+#   conf - the configuration dict
+#   parts - the particle array
+#   cells - an N dimensional array of cells
+function assignParts(s::System)
+  cSize = s.cellGrid.cellSize
+  # Clear all current particle lists for cells
+  for c in s.cellGrid.cells
+    c.parts = Array(Part, 0)
+  end
+  for p in s.parts
+    pos = p.pos + [s.dimConst.size, s.dimConst.size, s.dimConst.size]
+    hash = (div(pos[1], cSize) + div(pos[2], cSize)*s.cellGrid.cellNum
+            + div(pos[3], cSize)*s.cellGrid.cellNum^2 + 1)
+    hash = int(hash)
+    push!(s.cellGrid.cells[hash].parts, p)
+  end
+end
 
 function step(s::System)
   forceCalc(s::System)
-end
-
-function updateCells(s::System)
-
 end
