3d04cb68acea94b367903d8f8b7332d41291e8cc
Dan Kolbman <dankolbman@gmail.com>
2015-03-23 16:14:05 -0400

    Update formatting

--- doc/conf.py
+++ doc/conf.py
@@ -17,6 +17,9 @@ import sys
 import os
 import shlex
 
+# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
 # For Julia
 import juliadoc
 
@@ -37,6 +40,7 @@ import juliadoc
 extensions = [
     'sphinx.ext.autodoc',
     'juliadoc.julia',
+    'juliadoc.jldoctest',
     'juliadoc.jlhelp'
 ]
 
@@ -120,15 +124,19 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = 'julia'
+html_theme_path = [juliadoc.get_theme_dir()]
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'julia'
+    html_theme_path = [ juliadoc.get_theme_dir(),
+                        sphinx_rtd_theme.get_html_theme_path() ]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #html_theme_options = {}
 
-# Add any paths that contain custom themes here, relative to this directory.
-## Julia
-html_theme_path = [juliadoc.get_theme_dir()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
